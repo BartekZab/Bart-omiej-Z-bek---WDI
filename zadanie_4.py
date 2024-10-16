64492c9a391fd0695114df771ddd06e118966d79
BartekZab <bzabek@student.agh.edu.pl>
2024-10-16 20:25:28 +0200

    gotowe

---
+++ zadanie_4.py
@@ -0,0 +1,35 @@
+imie = "Bartlomiej"
+nazwisko = "Zabek"
+wiek = 19
+ulubiona_potrawa = "kebab"
+ulubione_zwierze = "malpa"
+x = 5/7
+
+#Format ze zmiennymi
+
+#a
+print(imie, nazwisko,"\n",x)
+
+#b
+print(ulubione_zwierze)
+
+#c
+print(x)
+print(round(x,1))
+print("%.3f"%x)
+print(round(x,5))
+print(round(x,10))
+
+#Format bez zmiennych 
+
+print("Bartlomiej,\n19,\nkebab")
+
+print("malpa")
+
+print(5/7)
+print(round(5/7,1))
+print(round(5/7,3))
+print(round(5/7,5))
+print(round(5/7,10))
+
+
