a4a2b529798d8cf753d105dd32568506065a9892
BartekZab <bzabek@student.agh.edu.pl>
2024-10-16 12:10:15 +0200

    Gotowe

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-print("Heloo world")
-print("Hello world 2 ")
+from random import randint
+
+los = randint(1,10)
+
+odp = -1
+
+i = 0 
+
+print("Zgadnij liczbe z przedzialu od 1 do 10")
+while odp != los:
+    i += 1
+    odp = int(input("POdaj liczbe: "))
+    if odp > los:
+        print("Niestety wylosowana liczba jest mniejsza od Twojej")
+    elif  odp < los:
+        print("Niestety wylosowana liczba jest wieksza od Twojej")
+print("Brawo odgadles liczbe za", i, "razem")
+
